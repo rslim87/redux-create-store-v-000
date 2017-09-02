ec7666adfce66aea9038a821a732dc89cb07e62c
github username <github email address>
2017-09-02 19:01:22 -0400

    Done.

--- reducer.js
+++ reducer.js
@@ -1,27 +1,46 @@
-let state;
-function changeState(state = {count: 0}, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+function createStore(reducer) {
+  let state;
+ 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
   }
-
-function dispatch(action){
-  state = changeState(state, action)
-  render()
+ 
+  function getState() {
+    return state;
+  };
+ 
+  dispatch({ type: '@@INIT' });
+ 
+  return {
+    dispatch, 
+    getState
+  };
+};
+ 
+function changeCount(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+ 
+    default:
+      return state;
+  }
 }
+ 
 
-function render(){
-  let container = document.getElementById('container')
-  container.textContent = state.count
-}
+let store = createStore(changeCount) // createStore takes the changeCount reducer as an argument
 
-  dispatch({type: '@@INIT'})
+function render() {
+  let container = document.getElementById('container');
+  container.textContent = store.getState.count;
+};
+
+
   
-  let button = document.getElementById('button');
 
-  button.addEventListener('click', function(){
-    dispatch({type: 'INCREASE_COUNT'})
-  })
+let button = document.getElementById('button');
+ 
+button.addEventListener('click', function() {
+  store.dispatch({ type: 'INCREASE_COUNT' });
+});
